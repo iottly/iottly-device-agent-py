5544f179516d999538f79973338a1c841bd08196
Stefano <stefano.terna@tomorrowdata.io>
2015-11-26 16:51:19 +0100

    fixed licences

--- iottly-device-agent-py/main.py
+++ iottly-device-agent-py/main.py
@@ -15,43 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
-"""
-
-BETA AGREEMENT
-
-OurCompany has developed RASPBERRY IOTTLY AGENT including modifications, 
-enhancements, improvements, updates, additions, derivative works, 
-documentation and related material ("Software"). 
-OurCompany desires that the Software be tested prior to general release.
-
-Licensee agrees that Software is the sole property of OurCompany 
-until it is officially released and includes valuable trade secrets of 
-OurCompany. Licensee agrees to treat Software as confidential and 
-will not without the express written authorization of OurCompany: 
-- Demonstrate, copy, sell or market Software to any third party; 
-- or Publish or otherwise disclose information relating to performance 
-  or quality of the Software to any third party; 
-- or Modify, reuse, disassemble, decompile, reverse engineer or otherwise 
-  translate Software or any portion thereof. 
-
-Software is provided "AS IS" without warranty of any kind. 
-The entire risk arising out of the use or performance of Software remains 
-with Licensee. In no event shall OurCompany be liable for any damage 
-whatsoever arising out of the use of or inability to use Software, 
-even if OurCompany has been advised of the possibility of such damages.
-
-"""
-
-__author__ = "Stefano Terna"
-__copyright__ = "Copyright 2015, Stefano Terna"
-__credits__ = []
-__license__ = "BETA AGREEMENT"
-
-__version__ = "0.6"
-__maintainer__ = "Stefano Terna"
-__email__ = "stefano.terna@tomorrowdata.io"
-__status__ = "Prototype"
-
 
 import time
 import json
