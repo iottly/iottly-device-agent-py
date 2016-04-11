244a1846994d1f62696c5b68f6fd039d898115dd
Stefano <stefano.terna@tomorrowdata.io>
2016-04-11 02:04:11 +0200

    raspberry-py self extracting installer

--- iottly-device-agent-py/install-service.sh
+++
@@ -1,5 +0,0 @@
-sudo cp iottly-device-agent.service /lib/systemd/system/iottly-device-agent.service
-sudo chmod 644 /lib/systemd/system/iottly-device-agent.service
-sudo systemctl daemon-reload
-sudo systemctl enable iottly-device-agent.service
-
